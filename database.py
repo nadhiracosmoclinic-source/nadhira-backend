4cb96e621cad04bb6aec9d0ef5cca9ca05cce095
nadhiracosmoclinic-source <nadhiracosmoclinic@gmail.com>
2026-06-21 00:44:55 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./clinic.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+autocommit=False,
+autoflush=False,
+bind=engine
 )
 
 Base = declarative_base()
