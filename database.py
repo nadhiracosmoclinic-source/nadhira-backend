0574ad1bc11f344d0447b5455e53ab24d5fca79c
nadhiracosmoclinic-source <nadhiracosmoclinic@gmail.com>
2026-06-25 21:47:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./clinic.db"
+)
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
 engine = create_engine(
 DATABASE_URL
